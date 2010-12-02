19ceef5339991777f9728f5d017d549a87355459
Travis Nesland <tnesland@gmail.com>
2010-12-02 16:58:41 -0500

    Initial commit (this is the second go'round).

---
+++ RRFPauseController.h
@@ -0,0 +1,180 @@
+////////////////////////////////////////////////////////////
+//  RRFPauseController.h
+//  RRFPause
+//  --------------------------------------------------------
+//  Author: Travis Nesland
+//  Created: 11/28/10
+//  Copyright 2010, Residential Research Facility,
+//  University of Kentucky. All Rights Reserved.
+/////////////////////////////////////////////////////////////
+#import <Cocoa/Cocoa.h>
+#import <TKUtility/TKUtility.h>
+
+@interface RRFPauseController : NSObject <TKComponentBundleLoading> {
+
+    // PROTOCOL MEMBERS
+    ///////////////////
+    NSDictionary *definition;
+    id delegate;
+    NSString *errorLog;
+    IBOutlet NSView *view;
+
+    // ADDITIONAL MEMBERS
+    /////////////////////
+    NSUInteger secondsToPause;
+    bool shouldStartFromRegValue;
+    IBOutlet NSTextField *timeDisplay;
+}
+
+// PROTOCOL PROPERTIES
+//////////////////////
+@property (assign) NSDictionary *definition;
+@property (assign) id <TKComponentBundleLoading> delegate;
+@property (nonatomic, retain) NSString *errorLog;
+@property (assign) IBOutlet NSView *view;
+
+// ADDITIONAL PROPERTIES
+////////////////////////
+@property (assign) IBOutlet NSTextField *timeDisplay;
+
+
+#pragma mark REQUIRED PROTOCOL METHODS
+
+/**
+ Start the component - will receive this message from the component controller
+ */
+- (void)begin;
+
+/**
+ Return full path to data directory as string
+ */
+- (NSString *)dataDirectory;
+
+/**
+ Return a string object representing all current errors in log form
+ */
+- (NSString *)errorLog;
+
+/**
+ Perform any and all error checking required by the component - return YES if
+ passed
+ */
+- (BOOL)isClearedToBegin;
+
+/**
+ Returns the file name containing the raw data that will be appended to the
+ data file
+ */
+- (NSString *)rawDataFile;
+
+/**
+ Perform actions required to recover from crash using the given raw data passed
+ as string
+ */
+- (void)recover;
+
+/**
+ Accept assignment for the component definition
+ */
+- (void)setDefinition: (NSDictionary *)aDictionary;
+
+/**
+ Accept assignment for the component delegate - The component controller will
+ assign itself as the delegate
+ Note: The new delegate must adopt the TKComponentBundleDelegate protocol
+ */
+- (void)setDelegate: (id <TKComponentBundleDelegate> )aDelegate;
+
+/**
+ Perform any and all initialization required by component - load any nib files
+ and perform all required initialization
+ */
+- (void)setup;
+
+/**
+ Return YES if component should perform recovery actions
+ */
+- (BOOL)shouldRecover;
+
+/**
+ Return the Task Name as string
+ */
+- (NSString *)taskName;
+
+/**
+ Perform any and all finalization required by component
+ */
+- (void)tearDown;
+
+/**
+ Return the main view that should be presented to the subject
+ */
+- (NSView *)mainView;
+
+
+
+
+#pragma mark OPTIONAL PROTOCOL METHODS
+// UNCOMMENT ANY OF THE FOLLOWING METHODS IF THEIR BEHAVIOR IS DESIRED
+//////////////////////////////////////////////////////////////////////
+
+/**
+ Run header if something other than default is required
+ */
+//- (NSString *)runHeader;
+
+/**
+ Session header if something other than default is required
+ */
+//- (NSString *)sessionHeader;
+
+/**
+ Summary data if desired
+ */
+//- (NSString *)summary;
+
+
+
+
+#pragma mark ADDITIONAL METHODS
+// PLACE ANY NON-PROTOCOL METHODS HERE
+//////////////////////////////////////
+
+/**
+ Add the error to an ongoing error log
+ */
+- (void)registerError: (NSString *)theError;
+
+
+
+
+#pragma mark Preference Keys
+// HERE YOU DEFINE KEY REFERENCES FOR ANY PREFERENCE VALUES
+// ex: extern NSString * const RRFPauseNameOfPreferenceKey;
+////////////////////////////////////////////////////////////
+extern NSString * const RRFPauseTaskNameKey;
+extern NSString * const RRFPauseDataDirectoryKey;
+
+
+
+
+#pragma mark Internal Strings
+// HERE YOU DEFINE KEYS FOR CONSTANT STRINGS
+////////////////////////////////////////////
+extern NSString * const RRFPauseMainNibNameKey;
+extern NSString * const RRFPauseDurationKey;
+extern NSString * const RRFPauseModeKey;
+
+
+
+
+#pragma mark Enumerated Values
+// HERE YOU CAN DEFINE ENUMERATED VALUES
+////////////////////////////////////////
+enum {
+  RRFPauseModeFromNow           = 0,
+  RRFPauseModeFromLastComponent = 1,
+  RRFPauseModeToNextWallClock   = 2
+};
+
+@end
---
+++ RRFPauseController.m
@@ -0,0 +1,226 @@
+////////////////////////////////////////////////////////////
+//  RRFPauseController.m
+//  RRFPause
+//  --------------------------------------------------------
+//  Author: Travis Nesland
+//  Created: 11/28/10
+//  Copyright 2010, Residential Research Facility,
+//  University of Kentucky. All Rights Reserved.
+/////////////////////////////////////////////////////////////
+#import "RRFPauseController.h"
+
+@implementation RRFPauseController
+
+// add any member that has a property
+@synthesize delegate,definition,errorLog,view,timeDisplay;
+
+// Definitions //
+/////////////////
+#define RRFPauseModeEval [[definition valueForKey:RRFPauseModeKey] unsignedIntegerValue]
+
+#pragma mark HOUSEKEEPING METHODS
+/**
+ Give back any memory that may have been allocated by this bundle
+ */
+- (void)dealloc {
+    [errorLog release];
+    // any additional release calls go here
+    // ------------------------------------
+    
+    [super dealloc];
+}
+
+#pragma mark REQUIRED PROTOCOL METHODS
+
+/**
+ Start the component - will receive this message from the component controller
+ */
+- (void)begin {
+  // TODO: schedule the end of the pause (register for notification)
+  // TODO: start a loop to update time prompt (non-precise, low processor)
+}
+
+/**
+ Return the full path to the data directory
+ (in this component we probably will not be logging data - but we will provide
+ jic)
+ */
+- (NSString *)dataDirectory {
+    return [[definition valueForKey:RRFPauseDataDirectoryKey]
+            stringByStandardizingPath];
+}
+
+/**
+ Return a string object representing all current errors in log form
+ */
+- (NSString *)errorLog {
+    return errorLog;
+}
+
+/**
+ Perform any and all error checking required by the component - return YES if
+ passed
+ */
+- (BOOL)isClearedToBegin {
+  return [errorLog length] == 0;
+}
+
+/**
+ Returns the file name containing the raw data that will be appended to the
+ data file
+ */
+- (NSString *)rawDataFile {
+    return [delegate defaultTempFile]; // this is the default implementation
+}
+
+/**
+ Perform actions required to recover from crash using the given raw data passed
+ as string
+ */
+- (void)recover {
+    // if no recovery is needed, nothing need be done here
+}
+
+/**
+ Accept assignment for the component definition
+ */
+- (void)setDefinition: (NSDictionary *)aDictionary {
+    definition = aDictionary;
+}
+/**
+ Accept assignment for the component delegate - The component controller will
+ assign itself as the delegate
+ Note: The new delegate must adopt the TKComponentBundleDelegate protocol
+ */
+- (void)setDelegate: (id <TKComponentBundleDelegate> )aDelegate {
+    delegate = aDelegate;
+}
+/**
+ Perform any and all initialization required by component - load any nib files
+ and perform all required initialization
+ */
+- (void)setup {
+
+  // CLEAR ERROR LOG
+  //////////////////
+  [self setErrorLog:@""];
+    
+  // --- WHAT NEEDS TO BE INITIALIZED BEFORE THIS COMPONENT CAN OPERATE? ---
+  ///////////////////////////////////////////////////////////////////////////
+  switch(RRFPauseModeEval) {
+      
+      case RRFPauseModeFromNow:
+        // TODO: seconds to pause = now + duration
+        secondsToPause = 
+        [[definition valueForKey:RRFPauseDurationKey] unsignedIntegerValue];
+        break;
+        
+      case RRFPauseModeFromLastComponent:
+        // TODO: get the time value from the registry file
+        [self registerError:@"Specified pause mode is not yet supported"];
+        break;
+
+      case RRFPauseModeToNextWallClock:
+        // TODO: determine the next wall clock interval
+        [self registerError:@"Specified pause mode is not yet supported"];
+        break;
+
+      default:
+        [self registerError:@"Invalid mode of operation"];
+  }
+  // TODO: check that time value is accecptable
+  if(NO) {
+    [self registerError:@"Invalid pause parameter"];
+  }
+    
+    // LOAD NIB
+    ///////////
+    if([NSBundle loadNibNamed:RRFPauseMainNibNameKey owner:self]) {
+        // SETUP THE INTERFACE VALUES
+        /////////////////////////////
+        
+    } else { // NIB DID NOT LOAD
+        [self registerError:@"Could not load Nib file"];
+    }
+}
+
+/**
+ Return YES if component should perform recovery actions
+ */
+- (BOOL)shouldRecover {
+    return NO;  // this is the default; change if needed
+}
+
+/**
+ Return the task name as string
+ */
+- (NSString *)taskName {
+  return [definition valueForKey:RRFPauseTaskNameKey];
+}
+
+/**
+ Perform any and all finalization required by component
+ */
+- (void)tearDown {
+    
+}
+/**
+ Return the main view that should be presented to the subject
+ */
+- (NSView *)mainView {
+    return view;
+}
+
+#pragma mark OPTIONAL PROTOCOL METHODS
+/** Uncomment and implement the following methods if desired */
+/**
+ Run header if something other than default is required
+ */
+//- (NSString *)runHeader {
+//
+//}
+/**
+ Session header if something other than default is required
+ */
+//- (NSString *)sessionHeader {
+//
+//}
+/**
+ Summary data if desired
+ */
+//- (NSString *)summary {
+//
+//}
+
+        
+        
+
+        
+#pragma mark ADDITIONAL METHODS
+/** Add additional methods required for operation */
+- (void)registerError: (NSString *)theError {
+    // append the new error to the error log
+    [self setErrorLog:[[errorLog stringByAppendingString:theError]
+                       stringByAppendingString:@"\n"]];
+}
+
+        
+
+#pragma mark Preference Keys
+// HERE YOU DEFINE KEY REFERENCES FOR ANY PREFERENCE VALUES
+// ex:
+// NSString * const RRFPauseNameOfPreferenceKey = @"RRFPauseNameOfPreference"
+NSString * const RRFPauseTaskNameKey = @"RRFPauseTaskName";
+NSString * const RRFPauseDataDirectoryKey = @"RRFPauseDataDirectory";
+NSString * const RRFPauseDurationKey = @"RRFPauseDuration";
+NSString * const RRFPauseModeKey = @"RRFPauseMode";
+
+
+
+#pragma mark Internal Strings
+// HERE YOU DEFINE KEYS FOR CONSTANT STRINGS //
+///////////////////////////////////////////////
+NSString * const RRFPauseMainNibNameKey = @"RRFPauseMainNib";
+
+
+@end
