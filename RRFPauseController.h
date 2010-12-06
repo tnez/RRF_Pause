b68161c243475dd0fa4201aec693d569c5606a04
Travis Nesland <tnesland@gmail.com>
2010-12-06 15:29:30 -0500

    Clean up indentation.

--- RRFPauseController.h
+++ RRFPauseController.h
@@ -12,18 +12,18 @@
 
 @interface RRFPauseController : NSObject <TKComponentBundleLoading> {
 
-    // PROTOCOL MEMBERS
-    ///////////////////
-    NSDictionary *definition;
-    id delegate;
-    NSString *errorLog;
-    IBOutlet NSView *view;
+  // PROTOCOL MEMBERS
+  ///////////////////
+  NSDictionary *definition;
+  id delegate;
+  NSString *errorLog;
+  IBOutlet NSView *view;
 
-    // ADDITIONAL MEMBERS
-    /////////////////////
-    NSUInteger secondsToPause;
-    bool shouldStartFromRegValue;
-    IBOutlet NSTextField *timeDisplay;
+  // ADDITIONAL MEMBERS
+  /////////////////////
+  NSUInteger secondsToPause;
+  bool shouldStartFromRegValue;
+  IBOutlet NSTextField *timeDisplay;
 }
 
 // PROTOCOL PROPERTIES
@@ -41,74 +41,74 @@
 #pragma mark REQUIRED PROTOCOL METHODS
 
 /**
- Start the component - will receive this message from the component controller
- */
+   Start the component - will receive this message from the component controller
+*/
 - (void)begin;
 
 /**
- Return full path to data directory as string
- */
+   Return full path to data directory as string
+*/
 - (NSString *)dataDirectory;
 
 /**
- Return a string object representing all current errors in log form
- */
+   Return a string object representing all current errors in log form
+*/
 - (NSString *)errorLog;
 
 /**
- Perform any and all error checking required by the component - return YES if
- passed
- */
+   Perform any and all error checking required by the component - return YES if
+   passed
+*/
 - (BOOL)isClearedToBegin;
 
 /**
- Returns the file name containing the raw data that will be appended to the
- data file
- */
+   Returns the file name containing the raw data that will be appended to the
+   data file
+*/
 - (NSString *)rawDataFile;
 
 /**
- Perform actions required to recover from crash using the given raw data passed
- as string
- */
+   Perform actions required to recover from crash using the given raw data passed
+   as string
+*/
 - (void)recover;
 
 /**
- Accept assignment for the component definition
- */
+   Accept assignment for the component definition
+*/
 - (void)setDefinition: (NSDictionary *)aDictionary;
 
 /**
- Accept assignment for the component delegate - The component controller will
- assign itself as the delegate
- Note: The new delegate must adopt the TKComponentBundleDelegate protocol
- */
+   Accept assignment for the component delegate - The component controller will
+   assign itself as the delegate
+   Note: The new delegate must adopt the TKComponentBundleDelegate protocol
+*/
 - (void)setDelegate: (id <TKComponentBundleDelegate> )aDelegate;
 
 /**
- Perform any and all initialization required by component - load any nib files
- and perform all required initialization
- */
+   Perform any and all initialization required by component - load any nib files
+   and perform all required initialization
+*/
 - (void)setup;
 
 /**
- Return YES if component should perform recovery actions
- */
+   Return YES if component should perform recovery actions
+*/
 - (BOOL)shouldRecover;
 
 /**
- Return the Task Name as string
- */
+   Return the Task Name as string
+*/
 - (NSString *)taskName;
 
 /**
- Perform any and all finalization required by component
- */
+   Perform any and all finalization required by component
+*/
 - (void)tearDown;
 
 /**
- Return the main view that should be presented to the subject
- */
+   Return the main view that should be presented to the subject
+*/
 - (NSView *)mainView;
 
 
@@ -119,18 +119,18 @@
 //////////////////////////////////////////////////////////////////////
 
 /**
- Run header if something other than default is required
- */
+   Run header if something other than default is required
+*/
 //- (NSString *)runHeader;
 
 /**
- Session header if something other than default is required
- */
+   Session header if something other than default is required
+*/
 //- (NSString *)sessionHeader;
 
 /**
- Summary data if desired
- */
+   Summary data if desired
+*/
 //- (NSString *)summary;
 
 
@@ -141,13 +141,13 @@
 //////////////////////////////////////
 
 /**
- Add the error to an ongoing error log
- */
+   Add the error to an ongoing error log
+*/
 - (void)registerError: (NSString *)theError;
 
 /**
- End the pause
- */
+   End the pause
+*/
 - (void)end;
 
 
--- RRFPauseController.m
+++ RRFPauseController.m
@@ -20,21 +20,21 @@
 
 #pragma mark HOUSEKEEPING METHODS
 /**
- Give back any memory that may have been allocated by this bundle
- */
+   Give back any memory that may have been allocated by this bundle
+*/
 - (void)dealloc {
-    [errorLog release];
-    // any additional release calls go here
-    // ------------------------------------
+  [errorLog release];
+  // any additional release calls go here
+  // ------------------------------------
     
-    [super dealloc];
+  [super dealloc];
 }
 
 #pragma mark REQUIRED PROTOCOL METHODS
 
 /**
- Start the component - will receive this message from the component controller
- */
+   Start the component - will receive this message from the component controller
+*/
 - (void)begin {
   [self performSelector:@selector(end)
              withObject:self afterDelay:secondsToPause];
@@ -42,64 +42,64 @@
 }
 
 /**
- Return the full path to the data directory
- (in this component we probably will not be logging data - but we will provide
- jic)
- */
+   Return the full path to the data directory
+   (in this component we probably will not be logging data - but we will provide
+   jic)
+*/
 - (NSString *)dataDirectory {
-    return [[definition valueForKey:RRFPauseDataDirectoryKey]
-            stringByStandardizingPath];
+  return [[definition valueForKey:RRFPauseDataDirectoryKey]
+           stringByStandardizingPath];
 }
 
 /**
- Return a string object representing all current errors in log form
- */
+   Return a string object representing all current errors in log form
+*/
 - (NSString *)errorLog {
-    return errorLog;
+  return errorLog;
 }
 
 /**
- Perform any and all error checking required by the component - return YES if
- passed
- */
+   Perform any and all error checking required by the component - return YES if
+   passed
+*/
 - (BOOL)isClearedToBegin {
   return [errorLog length] == 0;
 }
 
 /**
- Returns the file name containing the raw data that will be appended to the
- data file
- */
+   Returns the file name containing the raw data that will be appended to the
+   data file
+*/
 - (NSString *)rawDataFile {
-    return [delegate defaultTempFile]; // this is the default implementation
+  return [delegate defaultTempFile]; // this is the default implementation
 }
 
 /**
- Perform actions required to recover from crash using the given raw data passed
- as string
- */
+   Perform actions required to recover from crash using the given raw data passed
+   as string
+*/
 - (void)recover {
-    // if no recovery is needed, nothing need be done here
+  // if no recovery is needed, nothing need be done here
 }
 
 /**
- Accept assignment for the component definition
- */
+   Accept assignment for the component definition
+*/
 - (void)setDefinition: (NSDictionary *)aDictionary {
-    definition = aDictionary;
+  definition = aDictionary;
 }
 /**
- Accept assignment for the component delegate - The component controller will
- assign itself as the delegate
- Note: The new delegate must adopt the TKComponentBundleDelegate protocol
- */
+   Accept assignment for the component delegate - The component controller will
+   assign itself as the delegate
+   Note: The new delegate must adopt the TKComponentBundleDelegate protocol
+*/
 - (void)setDelegate: (id <TKComponentBundleDelegate> )aDelegate {
-    delegate = aDelegate;
+  delegate = aDelegate;
 }
 /**
- Perform any and all initialization required by component - load any nib files
- and perform all required initialization
- */
+   Perform any and all initialization required by component - load any nib files
+   and perform all required initialization
+*/
 - (void)setup {
 
   // CLEAR ERROR LOG
@@ -110,28 +110,28 @@
   ///////////////////////////////////////////////////////////////////////////
   switch(RRFPauseModeEval) {
       
-      case RRFPauseModeFromNow:
-        // TODO: seconds to pause = now + duration
-        secondsToPause = 
-        [[definition valueForKey:RRFPauseDurationKey] unsignedIntegerValue];
-      NSLog(@"Pause mode selected: RRFPauseModeFromNow");
-        break;
+  case RRFPauseModeFromNow:
+    // TODO: seconds to pause = now + duration
+    secondsToPause = 
+      [[definition valueForKey:RRFPauseDurationKey] unsignedIntegerValue];
+    NSLog(@"Pause mode selected: RRFPauseModeFromNow");
+    break;
         
-      case RRFPauseModeFromLastComponent:
-        // TODO: get the time value from the registry file
-        [self registerError:@"Specified pause mode is not yet supported"];
-      NSLog(@"Pause mode selected: RRFPauseModeFromLastComponent");
-        break;
+  case RRFPauseModeFromLastComponent:
+    // TODO: get the time value from the registry file
+    [self registerError:@"Specified pause mode is not yet supported"];
+    NSLog(@"Pause mode selected: RRFPauseModeFromLastComponent");
+    break;
 
-      case RRFPauseModeToNextWallClock:
-        // TODO: determine the next wall clock interval
-        [self registerError:@"Specified pause mode is not yet supported"];
-      NSLog(@"Pause mode selected: RRFPauseModeToNextWallClock");      
-        break;
+  case RRFPauseModeToNextWallClock:
+    // TODO: determine the next wall clock interval
+    [self registerError:@"Specified pause mode is not yet supported"];
+    NSLog(@"Pause mode selected: RRFPauseModeToNextWallClock");      
+    break;
 
-      default:
-        [self registerError:@"Invalid mode of operation"];
-      NSLog(@"Pause mode selected: invalid mode of operation");
+  default:
+    [self registerError:@"Invalid mode of operation"];
+    NSLog(@"Pause mode selected: invalid mode of operation");
   }
   // TODO: check that time value is accecptable
   if(NO) {
@@ -141,58 +141,58 @@
   // LOAD NIB
   ///////////
   if([NSBundle loadNibNamed:RRFPauseMainNibNameKey owner:self]) {
-      // SETUP THE INTERFACE VALUES
-      /////////////////////////////
+    // SETUP THE INTERFACE VALUES
+    /////////////////////////////
       
   } else { // NIB DID NOT LOAD
-      [self registerError:@"Could not load Nib file"];
+    [self registerError:@"Could not load Nib file"];
   }
 }
 
 /**
- Return YES if component should perform recovery actions
- */
+   Return YES if component should perform recovery actions
+*/
 - (BOOL)shouldRecover {
-    return NO;  // this is the default; change if needed
+  return NO;  // this is the default; change if needed
 }
 
 /**
- Return the task name as string
- */
+   Return the task name as string
+*/
 - (NSString *)taskName {
   return [definition valueForKey:RRFPauseTaskNameKey];
 }
 
 /**
- Perform any and all finalization required by component
- */
+   Perform any and all finalization required by component
+*/
 - (void)tearDown {
     
 }
 /**
- Return the main view that should be presented to the subject
- */
+   Return the main view that should be presented to the subject
+*/
 - (NSView *)mainView {
-    return view;
+  return view;
 }
 
 #pragma mark OPTIONAL PROTOCOL METHODS
 /** Uncomment and implement the following methods if desired */
 /**
- Run header if something other than default is required
- */
+   Run header if something other than default is required
+*/
 //- (NSString *)runHeader {
 //
 //}
 /**
- Session header if something other than default is required
- */
+   Session header if something other than default is required
+*/
 //- (NSString *)sessionHeader {
 //
 //}
 /**
- Summary data if desired
- */
+   Summary data if desired
+*/
 //- (NSString *)summary {
 //
 //}
@@ -204,14 +204,14 @@
 #pragma mark ADDITIONAL METHODS
 /** Add additional methods required for operation */
 - (void)registerError: (NSString *)theError {
-    // append the new error to the error log
-    [self setErrorLog:[[errorLog stringByAppendingString:theError]
+  // append the new error to the error log
+  [self setErrorLog:[[errorLog stringByAppendingString:theError]
                        stringByAppendingString:@"\n"]];
 }
 
 /**
- End the pause
- */
+   End the pause
+*/
 - (void)end {
   // notify our delegate that we are done
   [delegate componentDidFinish:self];
